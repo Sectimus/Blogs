4e055e18f6b6ad41c0589f442ea629a3f932aea7
30144129 <30144129@RXAS1002>
2017-11-20 09:30:46 +0000

    Add project files.

---
+++ Blogs/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blogs.Models
+{
+    public class Post
+    {
+
+        [Key]
+        private int PostID;
+
+        private DateTime DatePosted;
+
+        private DateTime DateEdited;
+
+        private String Title;
+
+        private String 
+    }
+}
